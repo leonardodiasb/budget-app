fbf83d2ce6dbd2995f9007f46bf8ecf121c301ef
leonardo <leonardodias@ucl.br>
2021-11-22 11:46:04 -0300

    Fix rubocop linters

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,5 +3,5 @@ class Entity < ApplicationRecord
   has_and_belongs_to_many :groups
 
   validates :name, presence: true
-  validates :amount, numericality: :greater_than_or_equal_to => 0
+  validates :amount, numericality: :greater_than_or_equal_to
 end
