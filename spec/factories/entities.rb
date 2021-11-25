595414e9951178e2e0ec146e1ca3fb3d8c66dc63
leonardo <leonardodias@ucl.br>
2021-11-25 07:20:43 -0300

    Create tests for entity and group pages.

--- spec/factories/entities.rb
+++ spec/factories/entities.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :entity do
-    name { Faker::String.random(length: 3..12) }
+    name { Faker::Movies::StarWars.character }
     amount { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
     user_id { Faker::Number.within(range: 1..10) }
   end
--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :group do
-    name { Faker::String.random(length: 3..12) }
+    name { Faker::Movies::StarWars.character }
     icon { 'Food' }
     user_id { Faker::Number.within(range: 1..10) }
   end
